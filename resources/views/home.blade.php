57febae2bda12a1024e96e1694636cc2b4b9d448
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-12 19:40:49 -0500

    Second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1122,7 +1122,7 @@
 
 
 	<!-- Banner video -->
-	<section class="parallax0 parallax100" style="background-image: url(images/bg-video-01.jpg);">
+	<section class="parallax0 parallax100" style="background-image: url(images/icons/portada.jpg);">
 		<div class="overlay0 p-t-190 p-b-200">
 			<div class="flex-col-c-m p-l-15 p-r-15">
 				<span class="m-text9 p-t-45 fs-20-sm">
