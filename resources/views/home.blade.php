db3d618121cfc22518f6b35d11b14a3203772bbc
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 16:01:42 -0500

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,496 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Hogar</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/themify/themify-icons.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/elegant-font/html-css/style.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/lightbox2/css/lightbox.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body class="animsition">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+	<!-- header -->
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+	@include("layouts/header")
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	<!-- Slide1 -->
+	<section class="slide1">
+		<div class="wrap-slick1">
+			<div class="slick1">
+
+			<!-- Abre foreach -->
+			<?php foreach($slides as $slide){?>
+
+				<div class="item-slick1 item1-slick1" style="background-image: url(images/master-slide-07.jpg);">
+					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+						<h2 class="caption1-slide1 xl-text2 t-center bo14 p-b-6 animated visible-false m-b-22" data-appear="fadeInUp">
+							<?php echo $slide-> titulo1; ?>
+						</h2>
+
+						<span class="caption2-slide1 m-text1 t-center animated visible-false m-b-33" data-appear="fadeInDown">
+							<?php echo $slide-> titulo2; ?>
+						</span>
+
+						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
+							<!-- Button -->
+							<a href="<?php echo $slide-> url; ?>" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+								Ver mas.
+							</a>
+						</div>
+					</div>
+				</div>
+
+			<!-- Cierro foreach -->
+			<?php } ?>
+
+			</div>
+		</div>
+	</section>
+
+	<!-- Our product -->
+	<section class="bgwhite p-t-45 p-b-58">
+		<div class="container">
+			<div class="sec-title p-b-22">
+				<h3 class="m-text5 t-center">
+					Our Products
+				</h3>
+			</div>
+
+			<!-- Tab01 -->
+			<div class="tab01">
+				<!-- Nav tabs -->
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link active" data-toggle="tab" href="#new-articles" role="tab">Recien Agregados</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-toggle="tab" href="#best-seller" role="tab">Mas Vendidos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-toggle="tab" href="#top-rate" role="tab">Mejor Calificacion</a>
+					</li>
+				</ul>
+
+				<!-- Tab panes -->
+				<div class="tab-content p-t-35">
+
+					<!-- - -->
+					<div class="tab-pane fade show active" id="new-articles" role="tabpanel">
+						<div class="row">
+
+						<!-- Abre foreach -->
+						<?php foreach($productos as $producto){?>
+
+							<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+								<!-- Block2 -->
+								<div class="block2">
+									<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+										<img src="images/item-02.jpg" alt="IMG-PRODUCT">
+
+										<div class="block2-overlay trans-0-4">
+											<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+												<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+												<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+											</a>
+
+											<div class="block2-btn-addcart w-size1 trans-0-4">
+												<!-- Button -->
+												<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+													Añadir al carrito
+												</button>
+											</div>
+										</div>
+									</div>
+
+									<div class="block2-txt p-t-20">
+										<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
+											<?php echo $producto-> titulo; ?>
+										</a>
+
+										<span class="block2-price m-text6 p-r-5">
+											<?php echo $producto-> precio; ?>
+										</span>
+									</div>
+								</div>
+							</div>
+
+						<!-- Cierro foreach -->
+						<?php } ?>
+
+						</div>
+					</div>
+
+					<!-- - -->
+					<div class="tab-pane fade" id="best-seller" role="tabpanel">
+						<div class="row">
+
+							<!-- Abre foreach -->
+							<?php foreach($productos as $producto){?>
+
+								<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+									<!-- Block2 -->
+									<div class="block2">
+										<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+											<img src="images/item-02.jpg" alt="IMG-PRODUCT">
+
+											<div class="block2-overlay trans-0-4">
+												<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+													<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+													<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+												</a>
+
+												<div class="block2-btn-addcart w-size1 trans-0-4">
+													<!-- Button -->
+													<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+														Añadir al carrito
+													</button>
+												</div>
+											</div>
+										</div>
+
+										<div class="block2-txt p-t-20">
+											<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
+												<?php echo $producto-> titulo; ?>
+											</a>
+
+											<span class="block2-price m-text6 p-r-5">
+												<?php echo $producto-> precio; ?>
+											</span>
+										</div>
+									</div>
+								</div>
+
+							<!-- Cierro foreach -->
+							<?php } ?>
+
+						</div>
+					</div>
+
+					<!--  -->
+					<div class="tab-pane fade" id="top-rate" role="tabpanel">
+						<div class="row">
+
+							<!-- Abre foreach -->
+							<?php foreach($productos as $producto){?>
+
+								<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+									<!-- Block2 -->
+									<div class="block2">
+										<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+											<img src="images/item-02.jpg" alt="IMG-PRODUCT">
+
+											<div class="block2-overlay trans-0-4">
+												<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+													<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+													<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+												</a>
+
+												<div class="block2-btn-addcart w-size1 trans-0-4">
+													<!-- Button -->
+													<button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+														Añadir al carrito
+													</button>
+												</div>
+											</div>
+										</div>
+
+										<div class="block2-txt p-t-20">
+											<a href="product-detail.html" class="block2-name dis-block s-text3 p-b-5">
+												<?php echo $producto-> titulo; ?>
+											</a>
+
+											<span class="block2-price m-text6 p-r-5">
+												<?php echo $producto-> precio; ?>
+											</span>
+										</div>
+									</div>
+								</div>
+
+							<!-- Cierro foreach -->
+							<?php } ?>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Banner video -->
+	<section class="parallax0 parallax100" style="background-image: url(images/icons/portada.jpg);">
+		<div class="overlay0 p-t-190 p-b-200">
+			<div class="flex-col-c-m p-l-15 p-r-15">
+				<span class="m-text9 p-t-45 fs-20-sm">
+					No lo mires.
+				</span>
+
+				<h3 class="l-text1 fs-35-sm">
+					PRUEBALO
+				</h3>
+
+				<span class="btn-play s-text4 hov5 cs-pointer p-t-25" data-toggle="modal" data-target="#modal-video-01">
+					<i class="fa fa-play" aria-hidden="true"></i>
+					Play Video
+				</span>
+			</div>
+		</div>
+	</section>
+
+	<!-- Blog -->
+	<section class="blog bgwhite p-t-94 p-b-65">
+		<div class="container">
+			<div class="sec-title p-b-52">
+				<h3 class="m-text5 t-center">
+					Our Blog
+				</h3>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="images/blog-01.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									Black Friday Guide: Best Sales & Discount Codes
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 22, 2017</span>
+
+							<p class="s-text8 p-t-16">
+								Duis ut velit gravida nibh bibendum commodo. Sus-pendisse pellentesque mattis augue id euismod. Inter-dum et malesuada fames
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="images/blog-02.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									The White Sneakers Nearly Every Fashion Girls Own
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 18, 2017</span>
+
+							<p class="s-text8 p-t-16">
+								Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit ame
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="images/blog-03.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									New York SS 2018 Street Style: Annina Mislin
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 2, 2017</span>
+
+							<p class="s-text8 p-t-16">
+								Proin nec vehicula lorem, a efficitur ex. Nam vehicula nulla vel erat tincidunt, sed hendrerit ligula porttitor. Fusce sit amet maximus nunc
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Instagram -->
+	<section class="instagram p-t-20">
+		<div class="sec-title p-b-52 p-l-15 p-r-15">
+			<h3 class="m-text5 t-center">
+				@embutidoslacuencana en Instagram
+			</h3>
+		</div>
+
+		<div class="flex-w">
+
+			<!-- Abre foreach -->
+			<?php $i=0; 
+			foreach($instagramfotos as $fotos){
+				if(++$i>4) break; ?>
+
+				<!-- Block4 -->
+				<div class="block4 wrap-pic-w">
+					<?php echo $fotos->url; ?>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</section>
+
+	
+	<!-- Shipping -->
+	<section class="shipping bgwhite p-t-62 p-b-46">
+		<div class="flex-w p-l-15 p-r-15">
+			<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
+				<h4 class="m-text12 t-center">
+					Free Delivery Worldwide
+				</h4>
+
+				<a href="#" class="s-text11 t-center">
+					Click here for more info
+				</a>
+			</div>
+
+			<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 bo2 respon2">
+				<h4 class="m-text12 t-center">
+					30 Days Return
+				</h4>
+
+				<span class="s-text11 t-center">
+					Simply return it within 30 days for an exchange.
+				</span>
+			</div>
+
+			<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
+				<h4 class="m-text12 t-center">
+					Store Opening
+				</h4>
+
+				<span class="s-text11 t-center">
+					Shop open from Monday to Sunday
+				</span>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Footer -->
+
+	@include("layouts/footer")
+
+	<!-- Back to top -->
+	<div class="btn-back-to-top bg0-hov" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<i class="fa fa-angle-double-up" aria-hidden="true"></i>
+		</span>
+	</div>
+
+	<!-- Container Selection1 -->
+	<div id="dropDownSelect1"></div>
+
+	<!-- Modal Video 01-->
+	<div class="modal fade" id="modal-video-01" tabindex="-1" role="dialog" aria-hidden="true">
+
+		<div class="modal-dialog" role="document" data-dismiss="modal">
+			<div class="close-mo-video-01 trans-0-4" data-dismiss="modal" aria-label="Close">&times;</div>
+
+			<div class="wrap-video-mo-01">
+				<div class="w-full wrap-pic-w op-0-0"><img src="images/icons/video-16-9.jpg" alt="IMG"></div>
+				<div class="video-mo-01">
+				
+					<iframe src="https://www.youtube.com/embed/be1lXeo62M8?rel=0&amp;showinfo=0" allowfullscreen></iframe>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
+	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+	<script type="text/javascript">
+		$(".selection-1").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/slick/slick.min.js"></script>
+	<script type="text/javascript" src="js/slick-custom.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/lightbox2/js/lightbox.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/sweetalert/sweetalert.min.js"></script>
+	<script type="text/javascript">
+		$('.block2-btn-addcart').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+			});
+		});
+
+		$('.block2-btn-addwishlist').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+			});
+		});
+	</script>
+
+<!--===============================================================================================-->
+	<script type="text/javascript" src="vendor/parallax100/parallax100.js"></script>
+	<script type="text/javascript">
+        $('.parallax100').parallax100();
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
