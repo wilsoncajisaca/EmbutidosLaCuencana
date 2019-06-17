9d476aef74383c9e6efd6d6c59a972e02c5f3e72
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 12:57:38 -0500

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,24 +46,29 @@
 		<div class="wrap-slick1">
 			<div class="slick1">
 
+
+			<?php foreach($slides as $slide){?>
+
 				<div class="item-slick1 item1-slick1" style="background-image: url(images/master-slide-07.jpg);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<h2 class="caption1-slide1 xl-text2 t-center bo14 p-b-6 animated visible-false m-b-22" data-appear="fadeInUp">
-							Leather Bags
+							<?php echo $slide-> titulo1; ?>
 						</h2>
 
 						<span class="caption2-slide1 m-text1 t-center animated visible-false m-b-33" data-appear="fadeInDown">
-							New Collection 2018
+							<?php echo $slide-> titulo2; ?>
 						</span>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
 							<!-- Button -->
 							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
+								Ver mas.
 							</a>
 						</div>
 					</div>
 				</div>
+
+			<?php } ?>
 
 			</div>
 		</div>
