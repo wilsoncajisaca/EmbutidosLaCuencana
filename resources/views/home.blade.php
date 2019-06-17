e9a70a0766f05d2cb7be15212d3fc4598b386eed
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 13:36:42 -0500

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,7 +46,7 @@
 		<div class="wrap-slick1">
 			<div class="slick1">
 
-
+			<!-- Abre foreach -->
 			<?php foreach($slides as $slide){?>
 
 				<div class="item-slick1 item1-slick1" style="background-image: url(images/master-slide-07.jpg);">
@@ -68,61 +68,12 @@
 					</div>
 				</div>
 
+			<!-- Cierro foreach -->
 			<?php } ?>
 
 			</div>
 		</div>
 	</section>
-
-	<!-- Banner -->
-	<div class="banner bgwhite p-t-40 p-b-40">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="images/banner-05.jpg" alt="IMG-BENNER">
-
-						<div class="block1-wrapbtn w-size2">
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Sunglasses
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="images/banner-03.jpg" alt="IMG-BENNER">
-
-						<div class="block1-wrapbtn w-size2">
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Watches
-							</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-					<!-- block1 -->
-					<div class="block1 hov-img-zoom pos-relative m-b-30">
-						<img src="images/banner-10.jpg" alt="IMG-BENNER">
-
-						<div class="block1-wrapbtn w-size2">
-							<!-- Button -->
-							<a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
-								Bags
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
 
 	<!-- Our product -->
 	<section class="bgwhite p-t-45 p-b-58">
