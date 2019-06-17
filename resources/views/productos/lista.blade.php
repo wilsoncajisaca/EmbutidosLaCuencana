f08460cec27ef2434f0c5ac7f99f37453f8aea1f
Wilson <wilsoncajisaca@gmail.com>
2019-06-17 01:11:20 -0500

    second

--- resources/views/productos/lista.blade.php
+++ resources/views/productos/lista.blade.php
@@ -68,19 +68,19 @@
 									{{$categoria->categoria}}
 								</a>
 							</li>
-							
+
 							@endforeach
 
 						</ul>
 
 						<!--  -->
 						<h4 class="m-text14 p-b-32">
-							Filters
+							Filtros
 						</h4>
 
 						<div class="filter-price p-t-22 p-b-50 bo3">
 							<div class="m-text15 p-b-17">
-								Price
+								Precio
 							</div>
 
 							<div class="wra-filter-bar">
@@ -91,12 +91,12 @@
 								<div class="w-size11">
 									<!-- Button -->
 									<button class="flex-c-m size4 bg7 bo-rad-15 hov1 s-text14 trans-0-4">
-										Filter
+										Filtro
 									</button>
 								</div>
 
 								<div class="s-text3 p-t-10 p-b-10">
-									Range: $<span id="value-lower">610</span> - $<span id="value-upper">980</span>
+									Rango: $<span id="value-lower">610</span> - $<span id="value-upper">980</span>
 								</div>
 							</div>
 						</div>
