08664a920e7c9905b0472698ada77c74b1255b4a
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 13:01:33 -0500

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,7 +61,7 @@
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
 							<!-- Button -->
-							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+							<a href="<?php echo $slide-> url; ?>" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
 								Ver mas.
 							</a>
 						</div>
