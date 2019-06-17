5f0b6332af241ee60e607dceb4d6fde0c9eb49f8
Wilson <wilsoncajisaca@gmail.com>
2019-06-17 00:46:04 -0500

    second

--- resources/views/productos/lista.blade.php
+++ resources/views/productos/lista.blade.php
@@ -56,39 +56,21 @@
 					<div class="leftbar p-r-20 p-r-0-sm">
 						<!--  -->
 						<h4 class="m-text14 p-b-7">
-							Categories
+							Categorias
 						</h4>
 
 						<ul class="p-b-54">
-							<li class="p-t-4">
-								<a href="#" class="s-text13 active1">
-									All
-								</a>
-							</li>
+
+							@foreach($categorias as $categoria)
 
 							<li class="p-t-4">
-								<a href="#" class="s-text13">
-									Women
+								<a href="{{$categoria->ruta}}" class="s-text13 active1">
+									{{$categoria->categoria}}
 								</a>
 							</li>
+							
+							@endforeach
 
-							<li class="p-t-4">
-								<a href="#" class="s-text13">
-									Men
-								</a>
-							</li>
-
-							<li class="p-t-4">
-								<a href="#" class="s-text13">
-									Kids
-								</a>
-							</li>
-
-							<li class="p-t-4">
-								<a href="#" class="s-text13">
-									Accesories
-								</a>
-							</li>
 						</ul>
 
 						<!--  -->
