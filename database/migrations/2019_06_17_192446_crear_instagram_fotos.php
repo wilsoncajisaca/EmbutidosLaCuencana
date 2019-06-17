8b77ff5d1cfc14b6d5b01390281f5922f3da75a9
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 14:30:59 -0500

    second

---
+++ database/migrations/2019_06_17_192446_crear_instagram_fotos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearInstagramFotos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instagrams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->i('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instagrams');
+    }
+}
