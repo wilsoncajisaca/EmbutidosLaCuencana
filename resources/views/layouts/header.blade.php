6c1cc8aad8d9266c6ddaa311a4cc64ef42288ae7
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 15:39:10 -0500

    second

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -338,12 +338,13 @@
 					<li class="item-menu-mobile">
 						<a href="productos">Categorias</a>
 						<ul class="sub_menu">
-									<li><a href="#">Carne de Ternero</a></li>
-									<li><a href="#">Carne de Porcino</a></li>
-									<li><a href="#">Carne de Cordero</a></li>
-									<li><a href="#">Vino</a></li>
-									<li><a href="#">Varios</a></li>
-								</ul>
+							<li><a href="#">Carne de Ternero</a></li>
+							<li><a href="#">Carne de Porcino</a></li>
+							<li><a href="#">Carne de Cordero</a></li>
+							<li><a href="#">Vino</a></li>
+							<li><a href="#">Varios</a></li>
+						</ul>
+						<i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
 					</li>
 
 					<li class="item-menu-mobile">
