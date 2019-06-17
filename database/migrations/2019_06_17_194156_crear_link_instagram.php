1a3b7dfd1dbea0d3c399cc7a22f38036de67bce1
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-17 14:42:32 -0500

    second

--- database/migrations/2019_06_17_194156_crear_link_instagram.php
+++ database/migrations/2019_06_17_194156_crear_link_instagram.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearLinksInstagram extends Migration
+class CrearLinkInstagram extends Migration
 {
     /**
      * Run the migrations.
