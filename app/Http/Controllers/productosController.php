608234c7965a0691aec885529f96bfc0b3a0040b
Wilson <wilsoncajisaca@gmail.com>
2019-06-17 00:54:30 -0500

    second

--- app/Http/Controllers/productosController.php
+++ app/Http/Controllers/productosController.php
@@ -15,8 +15,8 @@ class productosController extends Controller
      */
     public function index()
     {
-        $categorias=Categoria::all();
-        return view ("productos.lista",compact("categoria123"));
+        $categorias123=Categoria::all();
+        return view ("productos.lista",compact("categorias123"));
     }
 
     /**
