b4b35f484d89a1d21ee736a2daa006d4c05685b5
Wilson <wilsoncajisaca@gmail.com>
2019-06-18 22:20:39 -0500

    Second

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
 			<!-- Abre foreach Slide -->
 			<?php foreach($slides as $slide){?>
 
-				<div class="item-slick1 item1-slick1" style="background-image: url(<?php echo $slide-> imgFondo; ?>);">
+				<div class="item-slick1 item1-slick1" style="background-image: url({{$slide-> imgFondo}});">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<h2 class="caption1-slide1 xl-text2 t-center bo14 p-b-6 animated visible-false m-b-22" data-appear="fadeInUp">
 							<?php echo $slide-> titulo1; ?>
