41140275d88c872b76978fcfae15325483854d0e
Wilson <wilsoncajisaca@gmail.com>
2019-06-18 22:17:59 -0500

    Second

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,10 +46,10 @@
 		<div class="wrap-slick1">
 			<div class="slick1">
 
-			<!-- Abre foreach -->
+			<!-- Abre foreach Slide -->
 			<?php foreach($slides as $slide){?>
 
-				<div class="item-slick1 item1-slick1" style="background-image: url(images/master-slide-07.jpg);">
+				<div class="item-slick1 item1-slick1" style="background-image: url(<?php echo $slide-> imgFondo; ?>);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 						<h2 class="caption1-slide1 xl-text2 t-center bo14 p-b-6 animated visible-false m-b-22" data-appear="fadeInUp">
 							<?php echo $slide-> titulo1; ?>
@@ -68,7 +68,7 @@
 					</div>
 				</div>
 
-			<!-- Cierro foreach -->
+			<!-- Cierro foreach Slide-->
 			<?php } ?>
 
 			</div>
