0c4bc5178cb85c33ca9081433a82ad013b8521f0
Wilson <wilsoncajisaca@gmail.com>
2019-06-19 01:31:10 -0500

    Second

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -292,7 +292,7 @@
 								</a>
 							</h4>
 
-							<span class="s-text6">Publicado </span> <span class="s-text7">@php $date = date_create($blog->fecha); echo date_format($date, 'M D, Y'); @endphp</span>
+							<span class="s-text6">Publicado </span> <span class="s-text7">@php $date = date_create($blog->fecha); echo date_format($date, 'M d, Y'); @endphp</span>
 
 							<p class="s-text8 p-t-16">
 								{{$blog->descripcion}}
