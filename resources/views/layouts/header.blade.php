a8002ae084cf1b332fe034158d5257d7004bf233
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-19 12:08:11 -0500

    second

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,14 +6,13 @@
 				<div class="topbar-social">
 					<a href="#" class="topbar-social-item fa fa-facebook"></a>
 					<a href="#" class="topbar-social-item fa fa-instagram"></a>
-					<a href="#" class="topbar-social-item fa fa-pinterest-p"></a>
 					<a href="#" class="topbar-social-item fa fa-snapchat-ghost"></a>
 					<a href="#" class="topbar-social-item fa fa-youtube-play"></a>
 				</div>
 
 				<span class="topbar-child1">
-					@foreach(App\Bannerpromocione::orderBy('created_at','desc')->take(1)->get() as $work)
-						{{$work->descripcion}}
+					@foreach(App\Bannerpromocione::orderBy('created_at','desc')->take(1)->get() as $promocion)
+						{{$promocion->descripcion}}
 					@endforeach
 				</span>
 
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,4 @@ use App\Producto;
 Route::resource('/', 'homeController');
 Route::resource('/index', 'homeController');
 Route::resource('/productos', 'productosController');
-Route::resource('/header', 'headerController');
-/*Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');*/
+Route::resource('/blog', 'blogController');
