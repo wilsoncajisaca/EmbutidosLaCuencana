66f5c91bb19716f653dc6a98c3461b52dd657150
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-19 09:43:50 -0500

    second

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Producto;
 Route::resource('/', 'homeController');
 Route::resource('/index', 'homeController');
 Route::resource('/productos', 'productosController');
+Route::resource('/header', 'headerController');
 /*Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');*/
