14f991acb016822698595ca9b7e4043c41a4112e
Wilson <wilsoncajisaca@gmail.com>
2019-06-18 23:04:49 -0500

    Second

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -21,7 +21,7 @@ class homeController extends Controller
 
         $slide=Slide::all();
         $producto=Producto::all();
-        $fotosIns = DB::table(Linkinstagram)->take(5)->get();
+        $fotosIns = Linkinstagram::take(5);
         return view ("index",['slides'=>$slide, 'productos'=>$producto, 'instagramfotos'=>$fotosIns]);
     }
 
