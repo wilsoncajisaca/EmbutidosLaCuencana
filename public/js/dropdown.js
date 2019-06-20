23b98dba1d6701db15e0caa3ff25cf7eb2bc36ff
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-20 15:49:19 -0500

    second

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,3 +1,9 @@
 $(function(){
-    alert('Hoola')
+    $('select-categoria').on('change', onSelectChange);
+
 });
+
+function onSelectChange (){
+    var project_id = $(this).val();
+    alert(project_id);
+}
