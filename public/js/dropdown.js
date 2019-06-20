8a5017373b7680b7c9e3b33e404a39968ed030be
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-20 16:53:41 -0500

    second

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,17 +2,7 @@ $("#select-categoria").change(function(event){
     $.get("categoria/"+event.target.value+"", function(response,producto){
         $("#productos").empty();
         for(i=0; i<response.length; i++){
-            $("#productos").append("<div class='block2'>"); 
-            $("#productos").append("<div class='block2-img wrap-pic-w of-hidden pos-relative block2-labelnew'>");  
-            $("#productos").append("<img src='"+response[i].portada+"' alt='IMG-PRODUCT'>");
-            $("#productos").append("<div class='block2-overlay trans-0-4'>"); 
-            $("#productos").append("<a href='#' class='block2-btn-addwishlist hov-pointer trans-0-4'></a>"); 
-            $("#productos").append("<i class='icon-wishlist icon_heart_alt' aria-hidden='true'></i>");
-            $("#productos").append("<i class='icon-wishlist icon_heart dis-none' aria-hidden='true'></i>");
-            $("#productos").append("</a><div class='block2-btn-addcart w-size1 trans-0-4'>");
-            $("#productos").append("<button class='flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4'>Añadir al carrito</button></div></div></div>");
-            $("#productos").append("<div class='block2-txt p-t-20'><a href='#' class='block2-name dis-block s-text3 p-b-5'>"+response[i].titulo+"</a><span class='block2-price m-text6 p-r-5'>"+response[i].precio+"</span></div></div>");
-            
+            $("#productos").append("<div class='block2'><div class='block2-img wrap-pic-w of-hidden pos-relative block2-labelnew'><img src='"+response[i].portada+"' alt='IMG-PRODUCT'><div class='block2-overlay trans-0-4'><a href='#' class='block2-btn-addwishlist hov-pointer trans-0-4'><i class='icon-wishlist icon_heart_alt' aria-hidden='true'></i><i class='icon-wishlist icon_heart dis-none' aria-hidden='true'></i></a><div class='block2-btn-addcart w-size1 trans-0-4'><button class='flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4'>Añadir al carrito</button></div></div></div><div class='block2-txt p-t-20'><a href='#' class='block2-name dis-block s-text3 p-b-5'>"+response[i].titulo+"</a><span class='block2-price m-text6 p-r-5'>"+response[i].precio+"</span></div></div>");
         }
         
     });
