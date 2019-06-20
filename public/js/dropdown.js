586c155cf1ed629b1c30b482ec710c086f4b0496
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-20 13:07:16 -0500

    second

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,5 +1,5 @@
 $("#categoria").change(function(event){
-    $.get("categoria/"+event.target.value+"",function(response,producto){
+    $.get("categoria/"+event.target.value+"", function(response,producto){
         console.log(response);
     });
 });
