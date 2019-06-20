d633ac39dec2f0f114a51fdc122a485f073137c6
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-20 17:48:09 -0500

    second

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -82,8 +82,8 @@
 									<li>
 										<a href="#">Perfil</a>
 										<ul class="sub_menu">
-											<li><a href="#">Ingresar</a></li>
-											<li><a href="#">Crea una cuenta</a></li>
+											<li> <button style="background-color: transparent" data-toggle="modal" data-target="#miModal" >Entrar</button></li>
+											<li> <button style="background-color: transparent" data-toggle="modal" data-target="#miModal" >Registrarme</button></li>
 										</ul>
 									</li>
 								</ul>
@@ -173,7 +173,21 @@
 			</div>
 		</div>
 
-
+			<div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<h4 class="modal-title" id="myModalLabel">Esto es un modal</h4>
+							</div>
+							<div class="modal-body">
+								Texto del modal
+							</div>
+						</div>
+					</div>
+				</div>
 
 
 		<!-- Header Mobile -->
