ea2bf23f41a782eef484a07e6c01071a44779fc7
Wilson <wilsoncajisaca@gmail.com>
2019-06-20 23:14:33 -0500

    second

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role=new Role();
+        $role->name="admin";
+        $role->descripcion="Administrador";
+        $role->save();
+
+        $role=new Role();
+        $role->name="user";
+        $role->descripcion="Usuario";
+        $role->save();
+
+    }
+}
