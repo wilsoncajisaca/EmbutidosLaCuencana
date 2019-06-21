dc0e7a5868a060c4dfe8858a776932c7901347f8
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-21 17:21:03 -0500

    second

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -36,7 +36,7 @@
     @include("layouts/header")
 
 	<!-- Title Page -->
-	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/heading-pages-06.jpg);">
+	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/banner_contactanos.jpg);">
 		<h2 class="l-text2 t-center">
 			Contact
 		</h2>
