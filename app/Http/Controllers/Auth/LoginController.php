ba251deb94b1f7ab295d5fd44f7d6d049bb431d9
Wilson <wilsoncajisaca@gmail.com>
2019-06-21 01:49:49 -0500

    make it better

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/index';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -34,6 +34,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->redirectTo = route ('/');
     }
 }
