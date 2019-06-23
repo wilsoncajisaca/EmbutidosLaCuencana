8301b985da7cd7f3967f36bd864b812d98e52c34
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 15:38:49 -0500

    make it better

--- resources/views/productos/producto_detail.blade.php
+++ resources/views/productos/producto_detail.blade.php
@@ -27,8 +27,8 @@
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('vendor/slick/slick.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{asset('css/util.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
 <body class="animsition">
