de55575a12fbb27cc695fc6c8dc157d4db92f9c4
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 16:57:52 -0500

    make it better

--- app/Http/Controllers/productosController.php
+++ app/Http/Controllers/productosController.php
@@ -84,11 +84,7 @@ class productosController extends Controller
 
         $productoDetail=Producto::find($id);
 
-            return view ("productos.producto_detail",
-                [
-                    'productoDetail'=>$productoDetail
-                ]
-        );
+        return view ("productos.producto_detail", compact("productoDetail"));
 
     }
 
