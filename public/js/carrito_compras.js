3449d7825ee82519006382bf1df664def7d9f42b
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 14:29:03 -0500

    make it better

--- public/js/carrito_compras.js
+++ public/js/carrito_compras.js
@@ -7,8 +7,11 @@ $(".agregarCarrito").click(function(){
 
     var idProducto=$(this).attr("idProducto");
     console.log("idProducto", idProducto);
-    var imagenn=$(this).attr("imagen");
+    var imagen=$(this).attr("imagen");
+    console.log("idProducto", imagen);
     var titulo=$(this).attr("tituloPro");
+    console.log("idProducto", titulo);
     var precio=$(this).attr("precioPro");
+    console.log("idProducto", precio);
 
 });
