4b571220cd2001e464409da2959e0dc6f5b37302
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 18:18:43 -0500

    make it better

--- resources/views/productos/producto_detail.blade.php
+++ resources/views/productos/producto_detail.blade.php
@@ -497,21 +497,21 @@
 		$('.block2-btn-addcart').each(function(){
 			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
 			$(this).on('click', function(){
-				swal(nameProduct, "is added to cart !", "success");
+				swal(nameProduct, "Añadido al carrito !", "success");
 			});
 		});
 
 		$('.block2-btn-addwishlist').each(function(){
 			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
 			$(this).on('click', function(){
-				swal(nameProduct, "is added to wishlist ffffff!", "success");
+				swal(nameProduct, "Añadido a favoritos !", "success");
 			});
 		});
 
 		$('.btn-addcart-product-detail').each(function(){
 			var nameProduct = $('.product-detail-name').html();
 			$(this).on('click', function(){
-				swal(nameProduct, "is added to wishlist !", "success");
+				swal(nameProduct, "Añadido al carrito !", "success");
 			});
 		});
 	</script>
