99883b1a692e4e50f11d9669c0a15e0f3ebdaba9
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 15:55:03 -0500

    make it better

--- resources/views/productos/producto_detail.blade.php
+++ resources/views/productos/producto_detail.blade.php
@@ -520,14 +520,14 @@
 
 
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/jquery/jquery-3.2.1.min.js}}"></script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/animsition/js/animsition.min.js}}"></script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
-	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/bootstrap/js/popper.js}}"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/bootstrap/js/bootstrap.min.js}}"></script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/select2/select2.min.js}}"></script>
 	<script type="text/javascript">
 		$(".selection-1").select2({
 			minimumResultsForSearch: 20,
@@ -540,10 +540,10 @@
 		});
 	</script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/slick/slick.min.js"></script>
-	<script type="text/javascript" src="js/slick-custom.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/slick/slick.min.js}}"></script>
+	<script type="text/javascript" src="{{ URL::tojs/slick-custom.js}}"></script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/sweetalert/sweetalert.min.js"></script>
+	<script type="text/javascript" src="{{ URL::tovendor/sweetalert/sweetalert.min.js}}"></script>
 	<script type="text/javascript">
 		$('.block2-btn-addcart').each(function(){
 			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
@@ -568,7 +568,7 @@
 	</script>
 
 <!--===============================================================================================-->
-	<script src="{{asset('js/main.js')}}"></script>
+	<script src="{{ URL::to('js/main.js') }} "></script>
 
 </body>
 </html>
