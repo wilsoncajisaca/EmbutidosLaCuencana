688cc6a7741f08337a7fda1b9ce72dd0fa29a1b1
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-24 10:30:41 -0500

    second

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -50,14 +50,6 @@
 			<div class="container-table-cart pos-relative">
 				<div class="wrap-table-shopping-cart bgwhite">
 					<table class="table-shopping-cart">
-						<tr class="table-head">
-							<th class="column-1"></th>
-							<th class="column-2">Product</th>
-							<th class="column-3">Price</th>
-							<th class="column-4 p-l-70">Quantity</th>
-							<th class="column-5">Total</th>
-						</tr>
-
 						<div class="table-row itemsProductos" >
 
 							<tr class="table-row sinProductos">
@@ -71,6 +63,14 @@
 							</tr>
 
 						</div>
+
+						<tr class="table-head">
+								<th class="column-1"></th>
+								<th class="column-2">Product</th>
+								<th class="column-3">Price</th>
+								<th class="column-4 p-l-70">Quantity</th>
+								<th class="column-5">Total</th>
+							</tr>
 
 					</table>
 				</div>
