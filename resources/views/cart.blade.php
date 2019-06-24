368aea21f98cb8a7ea4695af54937b4f8541610e
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-24 09:22:55 -0500

    second

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -59,13 +59,27 @@
 						</tr>
 
 						<tr class="table-row">
+							<td class="column-1">
+								<div class="cart-img-product b-rad-4 o-f-hidden">
+									<img src="images/item-10.jpg" alt="IMG-PRODUCT">
+								</div>
+							</td>
+							<td class="column-2">Men Tshirt</td>
+							<td class="column-3">$36.00</td>
+							<td class="column-4">
+								<div class="flex-w bo5 of-hidden w-size17">
+									<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+										<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+									</button>
 
-							<div class="productosItem"> 
+									<input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
 
-								Hola mundo
-	
-							</div>
-
+									<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+										<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+									</button>
+								</div>
+							</td>
+							<td class="column-5">$36.00</td>
 						</tr>
 
 					</table>
