20595ff78d8ec54fd9822fb5db9f446eb1ef67b7
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-24 09:30:23 -0500

    second

--- public/js/carrito_compras.js
+++ public/js/carrito_compras.js
@@ -11,35 +11,35 @@ if(localStorage.getItem("listaProductos")!=null){
 
     function funcionForEach(item, index){
 
-        $(".cuerpoCarrito").append(`
+        $(".itemsProductos").append(`
                                     <tr class="table-row">
-                                        <td class="column-1">
-                                            <div class="cart-img-product b-rad-4 o-f-hidden">
-                                                <img src="${item.imagen}" alt="IMG-PRODUCT">
-                                            </div>
-                                        </td>
-                                        <td class="column-2">${item.titulo}</td>
-                                        <td class="column-3">$${item.precio}</td>
-                                        <td class="column-4">
-                                            <div class="flex-w bo5 of-hidden w-size17">
-                                                <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-                                                    <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-                                                </button>
+                                    <td class="column-1">
+                                        <div class="cart-img-product b-rad-4 o-f-hidden">
+                                            <img src="images/item-10.jpg" alt="IMG-PRODUCT">
+                                        </div>
+                                    </td>
+                                    <td class="column-2">Men Tshirt</td>
+                                    <td class="column-3">$36.00</td>
+                                    <td class="column-4">
+                                        <div class="flex-w bo5 of-hidden w-size17">
+                                            <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+                                                <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+                                            </button>
 
-                                                <input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
+                                            <input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
 
-                                                <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-                                                    <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                        <td class="column-5">$${item.precio}</td>
-                                    </tr>
+                                            <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+                                                <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td class="column-5">$36.00</td>
+                                </tr>
                                 `)
         }
 
     }else{
-        
+
     }
 
 /*  
