65f902d6cfb6b236502f6214d675192c2a28cf0b
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-24 09:52:59 -0500

    second

--- public/js/carrito_compras.js
+++ public/js/carrito_compras.js
@@ -12,6 +12,7 @@ if(localStorage.getItem("listaProductos")!=null){
     function funcionForEach(item, index){
 
         $(".itemsProductos").append(`
+                                    <tr class="table-row">
                                     <td class="column-1">
                                         <div class="cart-img-product b-rad-4 o-f-hidden">
                                             <img src="images/item-10.jpg" alt="IMG-PRODUCT">
@@ -33,6 +34,7 @@ if(localStorage.getItem("listaProductos")!=null){
                                         </div>
                                     </td>
                                     <td class="column-5">$36.00</td>
+                                </tr>
                                 `)
         }
 
