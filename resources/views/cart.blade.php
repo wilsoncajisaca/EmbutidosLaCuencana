0cbca0cf0c1442b088b3d9b63eedae31ca5231a3
Wilson <wilsoncajisaca@gmail.com>
2019-06-23 20:52:25 -0500

    make it better

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -51,9 +51,9 @@
 					<table class="table-shopping-cart">
 						<tr class="table-head">
 							<th class="column-1"></th>
-							<th class="column-2">Product</th>
-							<th class="column-3">Price</th>
-							<th class="column-4 p-l-70">Quantity</th>
+							<th class="column-2">Producto</th>
+							<th class="column-3">Precio</th>
+							<th class="column-4 p-l-70">Cantidad</th>
 							<th class="column-5">Total</th>
 						</tr>
 
@@ -81,29 +81,6 @@
 							<td class="column-5">$36.00</td>
 						</tr>
 
-						<tr class="table-row">
-							<td class="column-1">
-								<div class="cart-img-product b-rad-4 o-f-hidden">
-									<img src="images/item-05.jpg" alt="IMG-PRODUCT">
-								</div>
-							</td>
-							<td class="column-2">Mug Adventure</td>
-							<td class="column-3">$16.00</td>
-							<td class="column-4">
-								<div class="flex-w bo5 of-hidden w-size17">
-									<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-									</button>
-
-									<input class="size8 m-text18 t-center num-product" type="number" name="num-product2" value="1">
-
-									<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-									</button>
-								</div>
-							</td>
-							<td class="column-5">$16.00</td>
-						</tr>
 					</table>
 				</div>
 			</div>
