1a43a1199a45a31c8aa32b4b0c519abf422345fd
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-27 18:37:09 -0500

    second

--- public/js/carrito_compras.js
+++ public/js/carrito_compras.js
@@ -3,16 +3,19 @@
     Visualizar los productos en la pagina del carrito de compras...
     ================================
 */
-if(localStorage.getItem("listaProductos")!=null){
+if(localStorage.getItem("listaProductos")!=null)
+{
 
     var listaCarrito = JSON.parse(localStorage.getItem("listaProductos"));
 
     listaCarrito.forEach(funcionForEach);
 
-//    $(".itemsProductos").empty();
+        //$(".itemsProductos").empty();
 
-    function funcionForEach(item, index){
-        $("#listProductos").append(`
+    function funcionForEach(item, index)
+        {
+
+        $(".listProductos").append(`
                                 <tr class="table-row">
                                     <td class="column-1">
                                         <div class="cart-img-product b-rad-4 o-f-hidden">
@@ -39,33 +42,7 @@ if(localStorage.getItem("listaProductos")!=null){
                                 `)
         }
 
-    }else{
-
-        $("#listProductos").append(`
-        <tr class="table-row">
-            <td class="column-1">
-                <div class="cart-img-product b-rad-4 o-f-hidden">
-                    <img src="images/item-10.jpg" alt="IMG-PRODUCT">
-                </div>
-            </td>
-            <td class="column-2">Men Tshirt</td>
-            <td class="column-3">$36.00</td>
-            <td class="column-4">
-                <div class="flex-w bo5 of-hidden w-size17">
-                    <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-                        <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-                    </button>
-
-                    <input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
-
-                    <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-                        <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </td>
-            <td class="column-5">$36.00</td>
-        </tr>
-        `)
+}else{
 
     }
 
