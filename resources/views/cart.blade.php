683abc0faab6124ed4cd52d529696b9825197a7c
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-27 18:15:27 -0500

    second

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -53,9 +53,9 @@
 
 						<tr class="table-head">
 							<th class="column-1"></th>
-							<th class="column-2">Product</th>
-							<th class="column-3">Price</th>
-							<th class="column-4 p-l-70">Quantity</th>
+							<th class="column-2">Producto</th>
+							<th class="column-3">Precio</th>
+							<th class="column-4 p-l-70">Cantidad</th>
 							<th class="column-5">Total</th>
 						</tr>
 						
