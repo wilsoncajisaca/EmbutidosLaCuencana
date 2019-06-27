68b326af1b8c758513a7cc827d1294b21b3fd13a
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-27 17:11:40 -0500

    second

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -225,6 +225,7 @@
 	</script>
 <!--===============================================================================================-->
 	<script src="{{ URL::to('js/main.js') }}"></script>
+	<script src="{{ URL::to('js/carrito_compras.js') }}"></script>
 
 </body>
 </html>
