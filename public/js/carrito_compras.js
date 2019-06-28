ffd5fed1bdb6b3a1230df0beaa42c1d266e5c2fa
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-27 19:21:01 -0500

    second

--- public/js/carrito_compras.js
+++ public/js/carrito_compras.js
@@ -3,49 +3,40 @@
     Visualizar los productos en la pagina del carrito de compras...
     ================================
 */
-if(localStorage.getItem("listaProductos")!=null)
-{
+if(localStorage.getItem("listaProductos")!=null){
 
     var listaCarrito = JSON.parse(localStorage.getItem("listaProductos"));
-    alert("Hello! I am an alert box!! "+listaCarrito);
-
 
     listaCarrito.forEach(funcionForEach);
 
-        //$(".itemsProductos").empty();
+    function funcionForEach(item, index){
 
-    function funcionForEach(item, index)
-        {
-            $(".ProductosItem").append(`
-                                <tr class="table-row">
-                                    <td class="column-1">
-                                        <div class="cart-img-product b-rad-4 o-f-hidden">
-                                            <img src="images/item-10.jpg" alt="IMG-PRODUCT">
-                                        </div>
-                                    </td>
-                                    <td class="column-2">Men Tshirt</td>
-                                    <td class="column-3">$36.00</td>
-                                    <td class="column-4">
-                                        <div class="flex-w bo5 of-hidden w-size17">
-                                            <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-                                                <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-                                            </button>
-
-                                            <input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
-
-                                            <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-                                                <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-                                            </button>
-                                        </div>
-                                    </td>
-                                    <td class="column-5">$36.00</td>
-                                </tr>
+        $(".cuerpoCarrito").append(`
+                                    <tr class="table-row">
+                                        <td class="column-1">
+                                            <div class="cart-img-product b-rad-4 o-f-hidden">
+                                                <img src="${item.imagen}" alt="IMG-PRODUCT">
+                                            </div>
+                                        </td>
+                                        <td class="column-2">${item.titulo}</td>
+                                        <td class="column-3">$${item.precio}</td>
+                                        <td class="column-4">
+                                            <div class="flex-w bo5 of-hidden w-size17">
+                                                <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+                                                    <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+                                                </button>
+                                                <input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
+                                                <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+                                                    <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+                                                </button>
+                                            </div>
+                                        </td>
+                                        <td class="column-5">$${item.precio}</td>
+                                    </tr>
                                 `)
         }
 
-}else{
-
-    alert("No tienes datos!!");
+    }else{
 
     }
 
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Carrito</title>
+	<title>Cart</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -42,7 +42,6 @@
 		</h2>
 	</section>
 
-
 	<!-- Cart -->
 	<section class="cart bgwhite p-t-70 p-b-100">
 		<div class="container">
@@ -50,19 +49,42 @@
 			<div class="container-table-cart pos-relative">
 				<div class="wrap-table-shopping-cart bgwhite">
 					<table class="table-shopping-cart">
-
 						<tr class="table-head">
 							<th class="column-1"></th>
-							<th class="column-2">Producto</th>
-							<th class="column-3">Precio</th>
-							<th class="column-4 p-l-70">Cantidad</th>
+							<th class="column-2">Product</th>
+							<th class="column-3">Price</th>
+							<th class="column-4 p-l-70">Quantity</th>
 							<th class="column-5">Total</th>
 						</tr>
 
-						<div class="ProductosItem" >
+						@foreach ($listaProductos as $productos)
+					
+							<tr class="table-row">
+									<td class="column-1">
+										<div class="cart-img-product b-rad-4 o-f-hidden">
+											<img src="images/item-10.jpg" alt="IMG-PRODUCT">
+										</div>
+									</td>
+									<td class="column-2">{{ $productos -> titulo }}</td>
+									<td class="column-3">${{ $productos -> precio }}</td>
+									<td class="column-4">
+										<div class="flex-w bo5 of-hidden w-size17">
+											<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
+												<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
+											</button>
+		
+											<input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
+		
+											<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
+												<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
+											</button>
+										</div>
+									</td>
+									<td class="column-5">${{ $productos -> total }}</td>
+								</tr>
 
-						</div>
-						
+						@endforeach
+
 					</table>
 				</div>
 			</div>
@@ -168,7 +190,6 @@
 		</div>
 	</section>
 
-
 	@include('layouts/footer')
 
 	<!-- Back to top -->
@@ -198,7 +219,6 @@
 			minimumResultsForSearch: 20,
 			dropdownParent: $('#dropDownSelect1')
 		});
-
 		$(".selection-2").select2({
 			minimumResultsForSearch: 20,
 			dropdownParent: $('#dropDownSelect2')
