f521fcbcefd4a5996c15787fe377eeb28c6664d8
Wilson <wilsoncajisaca@gmail.com>
2019-06-28 00:47:46 -0500

    make it better

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -94,7 +94,7 @@
 				<!--  -->
 				<div class="flex-w flex-sb-m p-b-12">
 					<span class="s-text18 w-size19 w-full-sm">
-						Subtotal:
+						Total sin IVA:
 					</span>
 
 					<span class="m-text21 w-size20 w-full-sm">
@@ -103,44 +103,25 @@
 				</div>
 
 				<!--  -->
-				<div class="flex-w flex-sb bo10 p-t-15 p-b-20">
+				<div class="flex-w flex-sb-m p-b-12">
 					<span class="s-text18 w-size19 w-full-sm">
-						Shipping:
+						IVA:
 					</span>
 
-					<div class="w-size20 w-full-sm">
-						<p class="s-text8 p-b-23">
-							There are no shipping methods available. Please double check your address, or contact us if you need any help.
-						</p>
+					<span class="m-text21 w-size20 w-full-sm">
+						12%
+					</span>
+				</div>
 
-						<span class="s-text19">
-							Calculate Shipping
-						</span>
+				<!--  -->
+				<div class="flex-w flex-sb-m p-b-12">
+					<span class="s-text18 w-size19 w-full-sm">
+						Gastos de envio:
+					</span>
 
-						<div class="rs2-select2 rs3-select2 rs4-select2 bo4 of-hidden w-size21 m-t-8 m-b-12">
-							<select class="selection-2" name="country">
-								<option>Select a country...</option>
-								<option>US</option>
-								<option>UK</option>
-								<option>Japan</option>
-							</select>
-						</div>
-
-						<div class="size13 bo4 m-b-12">
-						<input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="state" placeholder="State /  country">
-						</div>
-
-						<div class="size13 bo4 m-b-22">
-							<input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="postcode" placeholder="Postcode / Zip">
-						</div>
-
-						<div class="size14 trans-0-4 m-b-10">
-							<!-- Button -->
-							<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-								Update Totals
-							</button>
-						</div>
-					</div>
+					<span class="m-text21 w-size20 w-full-sm">
+						$5.00
+					</span>
 				</div>
 
 				<!--  -->
@@ -157,7 +138,7 @@
 				<div class="size15 trans-0-4">
 					<!-- Button -->
 					<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-						Proceed to Checkout
+						Proceder a pagar
 					</button>
 				</div>
 			</div>
