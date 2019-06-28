309d8066472ff57108673dd27347e3b2a3bf9171
wilson cajisaca <wilsoncajisaca@gmail.com>
2019-06-27 19:22:06 -0500

    second

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -57,33 +57,7 @@
 							<th class="column-5">Total</th>
 						</tr>
 
-						@foreach ($listaProductos as $productos)
-					
-							<tr class="table-row">
-									<td class="column-1">
-										<div class="cart-img-product b-rad-4 o-f-hidden">
-											<img src="images/item-10.jpg" alt="IMG-PRODUCT">
-										</div>
-									</td>
-									<td class="column-2">{{ $productos -> titulo }}</td>
-									<td class="column-3">${{ $productos -> precio }}</td>
-									<td class="column-4">
-										<div class="flex-w bo5 of-hidden w-size17">
-											<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-												<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-											</button>
-		
-											<input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
-		
-											<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-												<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-											</button>
-										</div>
-									</td>
-									<td class="column-5">${{ $productos -> total }}</td>
-								</tr>
-
-						@endforeach
+						<div class="cuerpoCarrito"></div>
 
 					</table>
 				</div>
